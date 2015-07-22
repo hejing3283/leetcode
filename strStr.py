d1288bb927849a2ad352d08c314e968df1bac94a
moonhj00 <violet.hj@gmail.com>
2015-07-22 09:02:52 -0400

    strStr brute force

---
+++ strStr.py
@@ -0,0 +1,40 @@
+def strStr( haystack, needle):
+  if not haystack :
+    if not needle :
+      return 0
+    else:
+      return -1
+  else:
+    if not needle: 
+      return 0
+    elif len(needle) > len(haystack):
+      return -1
+    else:
+      ihs = 0 
+      while ihs <  (len(haystack) - len(needle) + 1 ):
+	ind = 0; k = ihs
+	while ind <  len(needle):
+	  if haystack[k] == needle[ind]:
+	    ind += 1; k += 1
+	  else:
+	    break
+	if ind == len(needle) :
+	  break 
+	else:
+	  ihs += 1
+      if ihs == (len(haystack) - len(needle) + 1): 
+	return -1
+      else:
+	return ihs
+
+print 3,strStr( 'xyx567x', '56')
+print 0,strStr( 'xyx567x', '')
+print -1,strStr( 'xyx567x', '0')
+print 5,strStr( 'xyx567x', '7x')
+print 0,strStr( 'xyx567x', 'xy')
+print -1,strStr( '67x', 'xxxxy')
+print -1,strStr( '', 'xxxxy')
+print 0,strStr( '', '')
+print 0,strStr( '1', '1')
+
+
