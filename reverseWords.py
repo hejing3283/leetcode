fb3ff450ca8637677ce1184bc459b4c44b7ff959
moonhj00 <violet.hj@gmail.com>
2015-07-22 09:32:48 -0400

    finish

---
+++ reverseWords.py
@@ -0,0 +1,25 @@
+def reverseWords( s):
+  snew = []
+  n = len(s) 
+  _wd = []
+  if not s.strip():
+    return ""
+  else:
+    for ss in range(n):
+      if s[ss] != " ": 
+        _wd.append( s[ss])
+      elif len(_wd) >0  :
+        snew.insert(0,"".join(_wd) )
+        snew.insert(0," " )
+        _wd = []
+      else:
+	continue
+
+    snew.insert(0, "".join(_wd ))
+    return "".join(snew).strip()
+
+print "-",reverseWords('the  sky is blue'),"-"
+print "-",reverseWords('    '), "-"
+print "-",reverseWords('  , '),"-"
+print "-",reverseWords('   a   b '), "-"
+
