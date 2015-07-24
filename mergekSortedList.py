c389dce14413620b69a3e9d561025ba440357003
moonhj00 <violet.hj@gmail.com>
2015-07-24 14:09:37 -0400

    init merge k sorted list , linked list

---
+++ mergekSortedList.py
@@ -0,0 +1,40 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+def mergeKLists(lists):
+  
+  def mergeTwoLists( a, b ):
+    if not a and b:
+      return b
+    elif not b and a:
+      return a 
+    elif not a and not b:
+      return None
+    else:
+      if a.val > b.val:
+          a, b = b, a
+      a.next = mergeTwoLists(a.next, b)
+      return a or b
+      
+l1 = ListNode(-1)
+l1.next = ListNode(1)
+l1.next.next = ListNode(4)
+l1.next.next.next = ListNode(7)
+
+temp = ListNode(9)
+l2 = ListNode(8); l2.next = temp
+temp =ListNode(0); temp.next = l2
+l2 = temp
+
+temp = ListNode(15)
+l3 = ListNode(11); l3.next = temp
+temp =ListNode(-3); temp.next = l3
+l3 = temp
+
+out = mergekists( [l1, l2, l3] )
+while p :
+  print p.val,
+  p = p.next
+
