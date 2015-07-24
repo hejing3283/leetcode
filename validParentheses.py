19ca0e100236960a7e34e2c547d66abdf74eefb1
moonhj00 <violet.hj@gmail.com>
2015-07-23 21:40:24 -0400

    Working! use stack!

--- validParentheses.py
+++ validParentheses.py
@@ -1,31 +1,22 @@
 def isValid(s):
-  print 
-  pd = {}
+  if len(s) < 0 :
+    return True
+  if len(s) % 2 == 1:
+    return False
+  pd = {'(': ')', '[': ']', '{':'}' ,'<':'>'} 
+  p2 = []
   i = 0
-  ps = ['(', ')', '[', ']', '{', '}' ,'<', '>'] 
   while i < len(s) and s[i] :
     if s[i] in pd.keys():
-      pd[ s[i]].append( i )
-    else:
-      pd[ s[i] ] = [i]
-    i += 1
-  print pd
-  for p in range(0,len(ps),2):
-    p1 = ps[p];  p2 = ps[p+1]
-   
-    if (p2 in pd.keys())  +  (p1 in pd.keys() )== 1:
+      p2.insert(0, pd[ s[i] ])
+    elif  len(p2) > 0 and s[i] != p2.pop(0):
       return False
-    elif p2 in pd.keys() and p1 in pd.keys():
-      v1 = pd[p1]; v2 = pd[p2]
-      if len(v1) != len(v2) :
-	return False
-      for j in range(len(v2)) :
-	if v1[j] >=  v2[ j ]:
-	  return False
-
-  return True 
-
+    i += 1
+  return len(p2) == 0 
 print 'T', isValid( "()" )
 print 'T', isValid( "()[]{}" )
 print 'F', isValid( "([)]" )
 print 'F', isValid( "(]" )
+print 'T', isValid( "(())" )
+print 'T', isValid( "[" )
+print 'T', isValid( "[" )
