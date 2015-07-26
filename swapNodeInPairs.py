1a9d0e02d53630aacd095e698efc119f43b6834b
moonhj00 <violet.hj@gmail.com>
2015-07-26 00:11:05 -0400

    in progress

--- swapNodeInPairs.py
+++ swapNodeInPairs.py
@@ -5,5 +5,28 @@ class ListNode:
 def swapPairs(head):
   # @param {ListNode} head
   # @return {ListNode}
+  '''
+  no or one node
+
+  '''
+  if not head or not head.next :
+    return head 
+  
+  dummy = ListNode(-1)
+   
+  counter = 0
+  while head:
+    counter += 1
+    counter
+    first = head 
+    dummy.next = second 
+    second = head.next
+    head = second.next 
+
+    frist.next = dummy
+    second.next = first 
+
+
+    
 
 
