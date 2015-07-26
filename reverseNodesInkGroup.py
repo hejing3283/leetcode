d0cb3d91a42d486ca7c8114a91231ee28120c5e1
moonhj00 <violet.hj@gmail.com>
2015-07-25 22:24:55 -0400

    Working! need to review!

--- reverseNodesInkGroup.py
+++ reverseNodesInkGroup.py
@@ -6,48 +6,59 @@ class ListNode:
       while self:
 	print self.val,
 	self = self.next
-      print 
 
 # @param {ListNode} head
 # @param {integer} k
 # @return {ListNode}
 def reverseKGroup(head, k) :
-  def reverse(head):
-    ''' given the start node and end node '''
-    dummy = ListNode(0); 
-    while head :
-      curr = head.next
-      head.next = dummy.next
-      dummy.next = head
-      head = curr 
-    return dummy.next 
+  def reverse(prev, end ):
+    ''' given the start node and end node 
+     Reverse a link list between pre and next exclusively
+     an example:
+     a linked list:
+     0->1->2->3->4->5->6
+     |           |
+     pre        next
+     after call pre = reverse(pre, next)
+     0->3->2->1->4->5->6
+	      |  |
+	    pre next
+	
+    return the reversed list's last node, which is the precedence of parameter next
+    '''
+    curr = prev.next
+    temp = curr.next
+    while temp != end and temp :
+      curr.next = temp.next
+      temp.next = prev.next
+      prev.next = temp
+      temp = curr.next 
+    return curr 
 
-  if not head or not head.next or k < 2: return None
+  if not head or not head.next or k < 2: return head
   dummy = ListNode(-1); dummy.next = head; prev = dummy
   counter = 0
-  while head:
+  while head != None:
     counter += 1
     if  counter % k == 0 :
       prev = reverse(prev, head.next)
+      head = prev.next
     else:
       head = head.next
 
   return dummy.next
 
-a = [1,2,3,4,5 ] 
+a = [1,2,3,4,5] 
 l1 = ListNode(a[0])
 l1.next = ListNode(a[1])
 l1.next.next = ListNode(a[2])
 l1.next.next.next = ListNode(a[3])
 l1.next.next.next.next= ListNode(a[4])
 
-print "in", l1.pNodes()
 p = reverseKGroup( l1, 3)
-print "out 2 ", p.pNodes()
-print "in",l1.pNodes()
+print "out 3 ", l1.pNodes(), p.pNodes()
 p = reverseKGroup( l1, 2)
-print "out 3 ", p.pNodes()
-print "in", l1.pNodes()
-p = reverseKGroup( l1, 5)
-print "out 5 " , p.pNodes()
+print "out 2 ", p.pNodes()
+p = reverseKGroup( l1, 2)
+print "k=2: ",l1.pNodes() , p.pNodes()
 
