824b5b09717492fd3cd3176a60b8151c025f8ee5
moonhj00 <violet.hj@gmail.com>
2015-07-27 00:14:30 -0400

    AC! clean

--- substring_w_concat_words.py
+++ substring_w_concat_words.py
@@ -1,21 +1,15 @@
 def findSubstring(s, words):
-  
   if not words: return []
-
   N = len(s); n = len(words); k  = len(words[0])
-
   # if N < n*k: return []
-
   dwords = {}
   for w in words:
     dwords[w] = 1 if not w in dwords else dwords[w] + 1
 
-  ### methods 1:
   outIndices = []; 
   for i in range(0, N - n*k +1 ) :
     ss = s[i:i+n*k]
-    dsub = {}
-    j  = 0 
+    dsub = {}; j  = 0 
     while j < n:
       w = s[(i+k*j):(i+k*j + k)]
       if not w in dwords :
@@ -27,27 +21,6 @@ def findSubstring(s, words):
       j += 1
     if j == n:
       outIndices.extend([i])
- 
-  
-  # ### methods 2
-
-  # outIndices = []; left = 0
-  # for j in range(k):
-  #   left = j ;dsub = {}; count = 0
-  #   for i in range(j, N - n*k + 1, k ):
-  #     currw = s[i:i+k] 
-  #     if currw in dwords:
-	# dsub[currw] = 1 if not currw in dsub else dsub[currw] + 1
-	# count += 1
-	# while dsub[currw] > dwords[currw]:
-	  # dsub[s[left:left+k]] -= 1
-	  # left += k 
-	  # count -= 1
-	# if count == n :
-	  # outIndices.extend([left])
-  #     else: 
-	# left = i + k;  dsub = {};  count = 0
-
   return outIndices
 
 
