0e54886eddb0a8a54b7f2b434e40184324e420b3
moonhj00 <violet.hj@gmail.com>
2015-07-30 17:25:06 -0400

    empty input error

--- wildcard_matching.py
+++ wildcard_matching.py
@@ -1,32 +1,36 @@
 def isMatch(s, p):
-  pP = 0 ; pS = 0 
-  while s:
-    if p[0] == "*":
-      while p[0] == "*": ip +=1
-      p = p[ip-1:]
-    elif p[0] == '?': 
-      s = s[1:] ; 
-      p = p[1:]
-    else:
-      if s[0] == p[0] : s = s[1:]; p = p[1:]
-      else:
+  curS = 0 ; curP = 0
+  star = 0; ss = s[::] 
+  while curS < len(s) and curP < len(p):
+    print curS, curP, star,
+    if p[curP] == "*":
+      print 'case 1' 
+      star = curP; ss = s[curS::]; curP += 1
+      continue
+    if p[curP] == '?' or s[curS] == p[curP] : 
+      print 'case 2' 
+      curS += 1 ; curP += 1
+      continue
+    if star: 
+      print 'case star' 
+      curP = star + 1
+      curS += 1
+      s = ss[curS::]
+      continue
+    return False
 
-      for i in range( len(s)):
-          if len(p) > 1:
-	    s = s[i+1:]
-	    p = p[1:]
-            return isMatch(s, p)
-          else:
-            return True
-        else:
-	  return False
-      return False
+  if p[curP-1] == "*" : return True
+  if p[curP-1] == s[curS-1:]: return True 
+  return False 
   
 
-print  isMatch("aa","a")      ," false "  
-# print  isMatch("aa","aa")     ," true  "
-# print  isMatch("aaa","aa")    ," false "
-# print  isMatch("aa", "*")     ," true  "
-# print  isMatch("aa", "a*")    ," true  "
-# print  isMatch("ab", "?*")    ," true  "
-# print  isMatch("aab", "c*a*b")," false "
+# print  isMatch("aa","a")      ," False "  
+# print  isMatch("aa","aa")     ," True  "
+# print  isMatch("aaa","aa")    ," False "
+# print  isMatch("aa", "*")     ," True  "
+# print  isMatch("aa", "a*")    ," True  "
+# print  isMatch("ab", "?*")    ," True  "
+# print  isMatch("aab", "c*a*b")," False "
+# print  isMatch("abed", "?b*d**")," True "
+# print  isMatch("abefcdgiescdfimde", "ab*cd?i*de")," True "
+print  isMatch("", "")," True "
