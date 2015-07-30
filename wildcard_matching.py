9aefb661c6c074a6f399fb9a065d4293865f5e94
moonhj00 <violet.hj@gmail.com>
2015-07-29 21:53:51 -0400

    in progress

--- wildcard_matching.py
+++ wildcard_matching.py
@@ -1,26 +1,32 @@
 def isMatch(s, p):
-  if p[0] == "*":
-    if len(p) == 1: return True
-    else : isMatch(s[::], p[1::])
-  elif p[0] == '?':
-    if len(p) == 1: return True
-    else: isMatch(s[1::], p[1::])
-  else:
-    for i in range( len(s)):
-      if s[i] == p[0] :
-        if len(p) > 1:
-	  isMatch(s[i+1::], p[1::])
-        else:
-          return True
+  pP = 0 ; pS = 0 
+  while s:
+    if p[0] == "*":
+      while p[0] == "*": ip +=1
+      p = p[ip-1:]
+    elif p[0] == '?': 
+      s = s[1:] ; 
+      p = p[1:]
+    else:
+      if s[0] == p[0] : s = s[1:]; p = p[1:]
       else:
-	return False
-    return False
 
+      for i in range( len(s)):
+          if len(p) > 1:
+	    s = s[i+1:]
+	    p = p[1:]
+            return isMatch(s, p)
+          else:
+            return True
+        else:
+	  return False
+      return False
+  
 
 print  isMatch("aa","a")      ," false "  
-print  isMatch("aa","aa")     ," true  "
-print  isMatch("aaa","aa")    ," false "
+# print  isMatch("aa","aa")     ," true  "
+# print  isMatch("aaa","aa")    ," false "
 # print  isMatch("aa", "*")     ," true  "
-print  isMatch("aa", "a*")    ," true  "
-print  isMatch("ab", "?*")    ," true  "
+# print  isMatch("aa", "a*")    ," true  "
+# print  isMatch("ab", "?*")    ," true  "
 # print  isMatch("aab", "c*a*b")," false "
