36a38a64369a8e05f4abdb376a2a8d5330c4d9c0
moonhj00 <violet.hj@gmail.com>
2015-07-30 23:13:05 -0400

    init

---
+++ rotate_image.py
@@ -0,0 +1,12 @@
+class Solution(obj):
+  # @param {integer[][]} matrix
+  # @return {void} Do not return anything, modify matrix in-place instead.
+  def rotate(self, matrix):
+    n = len(matrix)
+
+
+	    
+
+sol = Solution()
+sol.rotate([[1,3,4],[1,2,3],[6,4,5]])
+
