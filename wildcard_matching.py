1fd45dc134ee6b4948abd8640abec24befff0917
moonhj00 <violet.hj@gmail.com>
2015-07-30 21:42:39 -0400

    AC. REVIEW!

--- wildcard_matching.py
+++ wildcard_matching.py
@@ -1,36 +1,44 @@
 def isMatch(s, p):
-  curS = 0 ; curP = 0
-  star = 0; ss = s[::] 
-  while curS < len(s) and curP < len(p):
-    print curS, curP, star,
-    if p[curP] == "*":
-      print 'case 1' 
-      star = curP; ss = s[curS::]; curP += 1
-      continue
-    if p[curP] == '?' or s[curS] == p[curP] : 
-      print 'case 2' 
-      curS += 1 ; curP += 1
-      continue
-    if star: 
-      print 'case star' 
-      curP = star + 1
-      curS += 1
-      s = ss[curS::]
+  curS = curP = ss = 0;  star = -1;  
+  while curS < len(s) :
+    if curP < len(p) :
+      if p[curP] == '?' or s[curS] == p[curP] : 
+      	curS += 1 ; curP += 1; continue
+    if curP < len(p) :
+      if p[curP] == "*" :
+	star = curP;  curP += 1; ss = curS; continue
+    if star != -1 : 
+      curP = star + 1;  ss += 1; curS = ss
       continue
     return False
-
-  if p[curP-1] == "*" : return True
-  if p[curP-1] == s[curS-1:]: return True 
+  
+  while curP < len(p) and p[curP] == "*": curP += 1 
+  if curP == len(p) : return True
   return False 
   
 
-# print  isMatch("aa","a")      ," False "  
-# print  isMatch("aa","aa")     ," True  "
-# print  isMatch("aaa","aa")    ," False "
-# print  isMatch("aa", "*")     ," True  "
-# print  isMatch("aa", "a*")    ," True  "
-# print  isMatch("ab", "?*")    ," True  "
-# print  isMatch("aab", "c*a*b")," False "
-# print  isMatch("abed", "?b*d**")," True "
-# print  isMatch("abefcdgiescdfimde", "ab*cd?i*de")," True "
+print  isMatch("aa","a")      ," False "  
+print  isMatch("aa","aa")     ," True  "
+print  isMatch("aaa","aa")    ," False "
+print  isMatch("aa", "*")     ," True  "
+print  isMatch("aa", "a*")    ," True  "
+print  isMatch("ab", "?*")    ," True  "
+print  isMatch("aab", "c*a*b")," False "
+print  isMatch("abed", "?b*d**")," True "
+print  isMatch("abefcdgiescdfimde", "ab*cd?i*de")," True "
 print  isMatch("", "")," True "
+print  isMatch("", "a")," False "
+print  isMatch("", "*")," True "
+print  isMatch("", "?")," False "
+print  isMatch("a", "a*")," True "
+print  isMatch("a", "aa")," False "
+print  isMatch("a", "?")," True "
+print  isMatch("a", "*")," True "
+print  isMatch("a", "")," False "
+print  isMatch("a", "?*")," True "
+print  isMatch("a", "?a*")," False "
+print  isMatch("a", "a?")," False "
+print  isMatch("a", "??")," False "
+print  isMatch("a", "**")," True "
+print  isMatch("a", "*?*")," True "
+print  isMatch("a", "?*?")," False "
