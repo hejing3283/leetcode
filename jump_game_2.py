3fa393348dfe39bfcfc9f25604bdf8883fb9ebad
moonhj00 <violet.hj@gmail.com>
2015-07-30 22:19:42 -0400

    AC. review

--- jump_game_2.py
+++ jump_game_2.py
@@ -1,6 +1,12 @@
 # @param {integer[]} nums
 # @return {integer}
-def jump(self, nums):
-  '''
-  -- DP: 
-  '''
+def jump(nums):
+  nsteps = curr = last = 0 
+  for i in range(len(nums)):
+    if i > last:
+      last = curr
+      nsteps += 1
+    curr = max(curr, i + nums[i])
+  return nsteps
+
+print jump([2,3,1,1,4]), 2
