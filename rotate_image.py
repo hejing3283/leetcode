1f79f5578d5001d0051b78941e4aad84c306fb77
moonhj00 <violet.hj@gmail.com>
2015-07-31 10:35:51 -0400

    AC

--- rotate_image.py
+++ rotate_image.py
@@ -1,24 +1,6 @@
 class Solution():
   # @param {integer[][]} M
   # @return {void} Do not return anything, modify M in-place instead.
-  # def rotate(self, M):
-  #   n = len(M) - 1
-  #   center = (n +1)/ 2  #+ n % 2
-  #   print center
-  #   for i in range(center  ):
-  #     for j in range(center ):
-	# tmp = M[i][j] 
-	# M[i][j]     = M[ n-i ][ j ]
-	# M[n-i][j]   = M[ n-i ][ n-j ]
-	# M[n-i][n-j] = M[i][n-j]
-	# M[i][n-j] = tmp 
-  #   if len(M) % 2 == 1:
-	# for i in range(center):
-	  # tmp = M[i][center] 
-	  # M[i][center]     = M[ n-center ][ i ]
-	  # M[n-center] [i]  = M[ n-i ][ n-center ]
-	  # M[n-i][n-center] = M[center][n-j]
-	  # M[center][n-j] = tmp 
   def rotate(self, M):
     n = len(M)
     for i in range(n):
@@ -26,7 +8,6 @@ class Solution():
 	M[i][j], M[j][i] = M[j][i], M[i][j]
     for i in range(n):
       M[i].reverse()
-    # print M
      
 
 
@@ -37,6 +18,6 @@ class Solution():
 
 sol = Solution()
 sol.rotate([[1,3,4,5],[1,2,3,1],[6,4,5,6],[2,3,1,1]])
-# sol.rotate([[1,3,4],[2,3,1],[6,5,6]])
+sol.rotate([[1,3,4],[2,3,1],[6,5,6]])
 sol.rotate([[1,1,1,1,1],[2,2,2,2,2],[3,3,3,3,3],[4,4,4,4,4],[5,5,5,5,5]])
 
