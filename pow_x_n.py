fae7c0d7c6d6c624de0fefab31f324f3ed6e0781
moonhj00 <violet.hj@gmail.com>
2015-08-02 13:30:05 -0400

    init

---
+++ pow_x_n.py
@@ -0,0 +1,11 @@
+class Solution:
+  # @param {float} x
+  # @param {integer} n
+  # @return {float}
+  def myPow(self, x, n):
+    '''
+    divide and conquer
+    overlow, sign, 0 
+    '''
+    return ans
+
