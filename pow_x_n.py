cefc65e9cad2c4649966c09e9d532f700e8f1d3f
moonhj00 <violet.hj@gmail.com>
2015-08-02 13:53:16 -0400

    fisrt try greedy

--- pow_x_n.py
+++ pow_x_n.py
@@ -5,7 +5,26 @@ class Solution:
   def myPow(self, x, n):
     '''
     divide and conquer
-    overlow, sign, 0 
+    overlow, underflow, sign, 0 
     '''
+    if abs(x) == 1: return (x * x if n%2 else x)
+    if x == 0 : return 0 
+    if n == 0: return 1 
+    ans = 0; 
+    if n < 0 : 
+      x = 1/x; n = abs(n)
+    while i <= n :
+      ans += x * x 
+      i += 2
+      if ans > OVERLOW:
+	return OVERFLOW
+      elif ans < UNDERFLOW:
+	return UNDERFLOW
+
     return ans
+
+sol = Solution()
+print sol.myPow(0.1,-0.1)
+print sol.myPow(10000,0)
+print sol.myPow(1,100)
 
