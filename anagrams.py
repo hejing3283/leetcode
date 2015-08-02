b69e1133a48f8bb060fa08928827b5f70fda7ddd
moonhj00 <violet.hj@gmail.com>
2015-08-02 13:12:21 -0400

    DFS output list of strings

--- anagrams.py
+++ anagrams.py
@@ -3,17 +3,23 @@ class Solution:
   # @return {string[]}
   def anagrams(self, strs):
     strs.sort()
-    if len(strs) == 0: return []
-    if len(strs) == 1: return [strs]
-    prev = ''; ans = []
+    strs = [ s for s in strs if s]
+    if len(strs) == 0: return [''] 
+    if len(strs) == 1: return strs
+    prev = None; ans = []
     for i in range(len(strs)):
-      if strs[i] == prev: 
-	prev = strs[i]
-	continue
+      if strs[i] == prev:  continue
+      prev = strs[i];
       for j in self.anagrams( strs[:i] + strs[i+1:]):
-	ans.append([strs[i]] + j ) 
+	ans.append(strs[i] + j) 
     return ans
 
 sol = Solution()
 print sol.anagrams(['a','b','c'])
+print sol.anagrams(['a','c','c'])
+print sol.anagrams(['a'])
+print sol.anagrams([''])
+print sol.anagrams(['',''])
+print sol.anagrams(['','a'])
+print sol.anagrams(['','a', '','c'])
 		        
