9431c03a11aada847338858d3c4746be3467cd4f
moonhj00 <violet.hj@gmail.com>
2015-08-04 09:03:14 -0400

    own thought solution working

--- pow_x_n.py
+++ pow_x_n.py
@@ -7,16 +7,19 @@ class Solution:
     divide and conquer
     overlow, underflow, sign, 0 
     '''
-    if abs(x) == 1: return (x * x if n%2 else x)
-    if x == 0 : return 0 
+    OVERFLOW = 200000000000
+    UNDERFLOW = -20000000000
     if n == 0: return 1 
-    ans = 0; 
-    if n < 0 : 
-      x = 1/x; n = abs(n)
+    if n < 0 : x = 1.0/x; n = abs(n); sign = -1
+    if n == 1: return x
+    if abs(x) == 1: return (x * x if n%2 else x)
+    if x == 0.0 or x == 0: return 0 
+
+    ans = 1; i = 1
     while i <= n :
-      ans += x * x 
+      ans = ans * x * x 
       i += 2
-      if ans > OVERLOW:
+      if ans > OVERFLOW:
 	return OVERFLOW
       elif ans < UNDERFLOW:
 	return UNDERFLOW
@@ -24,7 +27,10 @@ class Solution:
     return ans
 
 sol = Solution()
-print sol.myPow(0.1,-0.1)
-print sol.myPow(10000,0)
-print sol.myPow(1,100)
+# print sol.myPow(0.1,-1)
+# print sol.myPow(10000,0)
+# print sol.myPow(1,100)
+# print sol.myPow(3,2)
+# print sol.myPow(3,-2)
+print sol.myPow(2,-10)
 
