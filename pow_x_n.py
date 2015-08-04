7c79b65099c91db74dc7156e5761fd9c12cc8cce
moonhj00 <violet.hj@gmail.com>
2015-08-04 09:15:48 -0400

    working iterative

--- pow_x_n.py
+++ pow_x_n.py
@@ -7,30 +7,25 @@ class Solution:
     divide and conquer
     overlow, underflow, sign, 0 
     '''
-    OVERFLOW = 200000000000
-    UNDERFLOW = -20000000000
     if n == 0: return 1 
-    if n < 0 : x = 1.0/x; n = abs(n); sign = -1
+    if n < 0 : x = 1.0/x; n = - n; 
     if n == 1: return x
     if abs(x) == 1: return (x * x if n%2 else x)
     if x == 0.0 or x == 0: return 0 
 
-    ans = 1; i = 1
-    while i <= n :
-      ans = ans * x * x 
-      i += 2
-      if ans > OVERFLOW:
-	return OVERFLOW
-      elif ans < UNDERFLOW:
-	return UNDERFLOW
-
+    ans = 1; 
+    while n :
+      if n & 1:
+	ans = ans * x 
+      x *= x
+      n >>= 1
     return ans
 
 sol = Solution()
-# print sol.myPow(0.1,-1)
-# print sol.myPow(10000,0)
-# print sol.myPow(1,100)
-# print sol.myPow(3,2)
-# print sol.myPow(3,-2)
+print sol.myPow(0.1,-1)
+print sol.myPow(10000,0)
+print sol.myPow(1,100)
+print sol.myPow(3,2)
+print sol.myPow(3,-2)
 print sol.myPow(2,-10)
 
