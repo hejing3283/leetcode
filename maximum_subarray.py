633dade5b06c2cf76956f64138f94313e25ec2d6
moonhj00 <violet.hj@gmail.com>
2015-08-05 09:38:04 -0400

    n^2 brute force working

--- maximum_subarray.py
+++ maximum_subarray.py
@@ -2,3 +2,16 @@ class Solution:
   # @param {integer[]} nums
   # @return {integer}
   def maxSubArray(self, nums):
+    start = 0; end = 0
+    ans = None; 
+    for i in range( len(nums) ):
+      curr = nums[i] 
+      for j in range(i + 1, len(nums)): 
+	curr += nums[j] 
+	if curr > ans :
+	  ans = curr
+    return ans
+
+sol = Solution()
+print sol.maxSubArray([-2, 1, -3, 4,-1, 2, 1, -5, 4]) 
+
