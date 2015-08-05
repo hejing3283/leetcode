67f1b7f7516415368b8d90ff52790b3a4664646d
moonhj00 <violet.hj@gmail.com>
2015-08-05 09:22:23 -0400

    AC. review

--- n_queens_2.py
+++ n_queens_2.py
@@ -9,9 +9,33 @@ class Solution:
       return True
 
     ans = 0; 
-    depth = 0; curr = []
-    while depth < n :
+    board  = [-1 for _ in range(n)]
+    row = 0; col = 0
+    while row < n:
+      while col < n:
+	if is_QkinColumnJ(row, col):
+	  board[row] = col 
+	  col = 0
+	  break
+	else:
+	  col += 1
+      if board[ row ] == -1:
+	if row == 0:
+	  break
+	else:
+	  row -= 1
+	  col = board[row] + 1
+	  board[ row ] = -1
+	  continue 
+      if row == n-1:
+	ans += 1
+	col = board[row] + 1
+	board[row] = -1
+	continue 
+      row += 1
+    return  ans
 
+sol = Solution()
+print sol.totalNQueens(4)
+# print sol.totalNQueens(8)
 
-      
-    return 
