751c33d477228ce550840bbe98d0ec08c831954a
moonhj00 <violet.hj@gmail.com>
2015-08-04 21:50:05 -0400

    AC. review

--- n_queens.py
+++ n_queens.py
@@ -2,14 +2,29 @@ class Solution:
   # @param {integer} n
   # @return {string[][]}
   def solveNQueens(self, n):
-    if n < 1 or n == 2 or n == 3: return [[]]
-    if n == 1: return [['Q']]
+    def isQinColumnI(k , j ):
+      for i in range(k):
+	if board[i] == j or abs( board[i] - j) == abs(i - k) :
+	  return False
+      return True
 
+    def dfs(depth, curr ) :
+      if depth == n: ans.append(curr) ; return 
+      for i in range(n):
+	if isQinColumnI( depth, i ):
+	  board[ depth ]  = i 
+	  s = '.'*n
+	  dfs( depth + 1, curr + [s[:i] + 'Q' + s[i+1:]] )
+    
+    board = [ -1 for _ in range(n) ] 
+    ans = []; 
+    dfs(0, [])
     return ans 
 
 
 sol = Solution()
-print [[".Q..",  "...Q", "Q...", "..Q."], ["..Q.", "Q...", "...Q", ".Q.."]],
+print [[".Q..",  "...Q", "Q...", "..Q."], ["..Q.", "Q...", "...Q", ".Q.."]]
 print sol.solveNQueens(4)
-print sol.solveNQueens(8)
+print "---------------"
+# print sol.solveNQueens(8)
 
