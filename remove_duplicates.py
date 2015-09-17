9a0daa942793c49cae7a95d54a46225adf1933d6
moonhj00 <violet.hj@gmail.com>
2015-09-16 22:10:01 -0400

    AC self

---
+++ remove_duplicates.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        if len(nums) < 3 : return len(nums)
+        i=2; 
+        while i < len(nums):
+            if nums[i] == nums[i-1]:
+                if nums[i] == nums[i-2]:
+                    nums.pop(i)
+                else:
+                    i += 1
+            else:
+                i += 1
+        return len(nums)
+                    
