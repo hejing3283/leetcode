e372eb28d9d5a34136b127c3cb9458347367f910
moonhj00 <violet.hj@gmail.com>
2015-09-22 12:15:32 -0400

    AC recursive

--- binary_tree_zigzag_level_order.py
+++ binary_tree_zigzag_level_order.py
@@ -6,35 +6,53 @@
 #         self.right = None
 
 class Solution(object):
+    # def zigzagLevelOrder(self, root):
+    #     """
+    #     :type root: TreeNode
+    #     :rtype: List[List[int]]
+    #     """
+        
+    #     ### still struggle with the direction problem 
+    #     q1, q2 = [], [] 
+    #     out = [] 
+    #     if not root: return [] 
+    #     q1.append(root) 
+    #     level  = 0 
+    #     while root or q1:
+    #         curr = [] 
+    #         size = len(q1)
+    #         if level % 2 == 1 : q1 = q1[::-1] 
+    #         for i in range(len(q1)):
+    #             root = q1.pop(0)
+    #             if root !=None :
+    #                 curr.append(root.val)
+    #                 if level % 2 == 0:
+    #                     q2.append(root.left); q2.append(root.right)
+    #                 else:
+    #                     q2.insert(0, root.right); q2.insert(0,root.left);
+    #         if level % 2 == 1 : print "here", ; q2[::] == q2[::-1]
+    #         q1[::] = q2;  
+    #         q2 = [] 
+    #         level += 1 
+    #         if curr:
+    #             out.append(curr)
+    #     return out
+                
     def zigzagLevelOrder(self, root):
         """
         :type root: TreeNode
         :rtype: List[List[int]]
+        : solution 2
         """
-        
-        ### still struggle with the direction problem 
-        q1, q2 = [], [] 
-        out = [] 
-        if not root: return [] 
-        q1.append(root) 
-        level  = 0 
-        while root or q1:
-            curr = [] 
-            size = len(q1)
-            if level % 2 == 1 : q1 = q1[::-1] 
-            for i in range(len(q1)):
-                root = q1.pop(0)
-                if root !=None :
-                    curr.append(root.val)
-                    if level % 2 == 0:
-                        q2.append(root.left); q2.append(root.right)
-                    else:
-                        q2.insert(0, root.right); q2.insert(0,root.left);
-            if level % 2 == 1 : print "here", ; q2[::] == q2[::-1]
-            q1[::] = q2;  
-            q2 = [] 
-            level += 1 
-            if curr:
-                out.append(curr)
-        return out
-                
+        def bfs(root, level, res):
+            if len(res) < level + 1: res.append([])
+            if root:
+                if level % 2 == 0 : res[level].append(root.val)
+                else:res[level].insert(0,root.val)
+                bfs(root.left, level + 1, res)
+                bfs(root.right, level + 1, res) 
+        res = []
+        bfs(root, 0, res )
+        res = [ x for x in res if x ] 
+        return res
+            
